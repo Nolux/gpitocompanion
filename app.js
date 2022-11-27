7f7fcc900aeb33480cc83ede5c7f23202b34cbfd
Atle Selbek <atle.selbek@gmail.com>
2022-11-27 23:08:47 +0000

    bugfix

--- app.js
+++ app.js
@@ -170,7 +170,7 @@ app.get("/", function (req, res) {
 app.get("/fallbackToggle", (req, res) => {
   fallback = !fallback;
   if (settings.debug) {
-    console.log("Fallback is: ", fallback);
+    consoleLog(`Fallback is: ${fallback}`);
   }
   res.json(fallback);
 });
